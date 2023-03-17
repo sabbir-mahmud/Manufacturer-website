24b2cce8100a5699701dd2db119806be017e7122
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-03-17 08:03:28 +0600

    new server linked added

--- src/Components/Pages/Home/HomeProducts.js
+++ src/Components/Pages/Home/HomeProducts.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import Loading from '../../Shared/Loading/Loading';
-import HomeProduct from './HomeProduct';
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../../Shared/Loading/Loading";
+import HomeProduct from "./HomeProduct";
 
 const HomeProducts = () => {
-    const { data: products, isLoading } = useQuery('products', () => {
-        return fetch('https://young-garden-78103.herokuapp.com/api/home/products')
-            .then(response => response.json())
-    });
-    return (
-        <div>
-            <div className='mb-24'>
-                <h2 className='text-4xl text-primary font-bold text-center'>Our Products</h2>
-            </div>
-            <div className="my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-                {
-                    isLoading ? <Loading /> : products?.map(product => <HomeProduct
-                        key={product._id}
-                        product={product}
-                    >
-                    </HomeProduct>)
-                }
-            </div>
-
-        </div>
+  const { data: products, isLoading } = useQuery("products", () => {
+    return fetch("http://localhost:5000/api/products/").then((response) =>
+      response.json()
     );
+  });
+  return (
+    <div>
+      <div className="mb-24">
+        <h2 className="text-4xl text-primary font-bold text-center">
+          Our Products
+        </h2>
+      </div>
+      <div className="my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          products?.map((product) => (
+            <HomeProduct key={product._id} product={product}></HomeProduct>
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HomeProducts;
