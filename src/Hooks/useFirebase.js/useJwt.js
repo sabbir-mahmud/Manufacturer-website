cc03eff8f17c6752077268b45064e0628461ac56
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-03-24 09:22:42 +0600

    updated edit profile routed created

--- src/Hooks/useFirebase.js/useJwt.js
+++ src/Hooks/useFirebase.js/useJwt.js
@@ -1,19 +1,18 @@
 const generateToken = async (email) => {
-    const url = `https://young-garden-78103.herokuapp.com/api/login`
-    fetch(url, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            const token = data.accessToken;
-            localStorage.setItem('accessToken', token);
-        });
-
+  const url = `http://localhost:5000/api/users/login`;
+  fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const token = data.accessToken;
+      localStorage.setItem("accessToken", token);
+    });
 };
 
 export default generateToken;
