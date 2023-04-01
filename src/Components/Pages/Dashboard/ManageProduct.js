3325f38ab2e9ca77f83eae5b0f5e8cbb4b1c9fe6
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-04-01 10:30:46 +0600

    latest routes added

--- src/Components/Pages/Dashboard/ManageProduct.js
+++ src/Components/Pages/Dashboard/ManageProduct.js
@@ -1,74 +1,93 @@
-import React, { useState } from 'react';
-import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import useUser from '../../../Hooks/useFirebase.js/useUser';
-import DeleteProductModal from './DeleteProductModal';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import useUser from "../../../Hooks/useFirebase.js/useUser";
+import DeleteProductModal from "./DeleteProductModal";
 
 const ManageProduct = () => {
-    const { handleLogout } = useUser();
-    const { data: products, refetch } = useQuery('manageProducts', () => fetch('https://young-garden-78103.herokuapp.com/api/products').then(res => res.json()));
-    const [delProduct, setDelProduct] = useState({});
+  const { handleLogout } = useUser();
+  const { data: products, refetch } = useQuery("manageProducts", () =>
+    fetch("http://localhost:5000/api/products/").then((res) => res.json())
+  );
+  const [delProduct, setDelProduct] = useState({});
 
-    const handleDelete = (id) => {
-        const url = `https://young-garden-78103.herokuapp.com/api/products/${id}`;
-        fetch(url, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "authorization": `Bearer ${localStorage.getItem("accessToken")}`
-            }
-        }).then(res => {
-            if (res.status === 401 || res.status === 403) {
-                handleLogout();
-                return toast.error('You are not authorized to perform this action');
-            } else {
-                return res.json();
-
-            }
-        })
-            .then(data => console.log(data))
-        refetch()
-        setDelProduct({});
-        toast.info('Product deleted successfully');
-
-    }
-    return (
-        <div>
-            <table className="table w-full">
-                {delProduct._id && <DeleteProductModal delProduct={delProduct} handleDelete={handleDelete} />}
-                <thead>
-                    <tr>
-                        <th>Product Name</th>
-                        <th>model</th>
-                        <th>type</th>
-                        <th>price</th>
-                        <th>Quantity</th>
-                        <th>edit</th>
-                        <th>delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products && products.map(product => {
-                            return (
-                                <tr key={product._id} className="hover">
-                                    <td>{product.name}</td>
-                                    <td>{product.model}</td>
-                                    <td>{product.type}</td>
-                                    <td>{product.price}</td>
-                                    <td>{product.quantity}</td>
-                                    <td><Link className='text-orange-500 hover:cursor-pointer' to={`/dashboard/manage-Product/${product._id}`}>Update</Link></td>
-                                    <td><label className='text-red-500 hover:underline hover:cursor-pointer' onClick={() => setDelProduct(product)} htmlFor="delete-product-admin">delete</label></td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-
-        </div>
-    );
+  const handleDelete = (id) => {
+    const url = `https://young-garden-78103.herokuapp.com/api/products/${id}`;
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 401 || res.status === 403) {
+          handleLogout();
+          return toast.error("You are not authorized to perform this action");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => console.log(data));
+    refetch();
+    setDelProduct({});
+    toast.info("Product deleted successfully");
+  };
+  return (
+    <div>
+      <table className="table w-full">
+        {delProduct._id && (
+          <DeleteProductModal
+            delProduct={delProduct}
+            handleDelete={handleDelete}
+          />
+        )}
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>model</th>
+            <th>type</th>
+            <th>price</th>
+            <th>Quantity</th>
+            <th>edit</th>
+            <th>delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products &&
+            products.map((product) => {
+              return (
+                <tr key={product._id} className="hover">
+                  <td>{product.name}</td>
+                  <td>{product.model}</td>
+                  <td>{product.type}</td>
+                  <td>{product.price}</td>
+                  <td>{product.quantity}</td>
+                  <td>
+                    <Link
+                      className="text-orange-500 hover:cursor-pointer"
+                      to={`/dashboard/manage-Product/${product._id}`}
+                    >
+                      Update
+                    </Link>
+                  </td>
+                  <td>
+                    <label
+                      className="text-red-500 hover:underline hover:cursor-pointer"
+                      onClick={() => setDelProduct(product)}
+                      htmlFor="delete-product-admin"
+                    >
+                      delete
+                    </label>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ManageProduct;
--- src/Components/Pages/Dashboard/Users.js
+++ src/Components/Pages/Dashboard/Users.js
@@ -1,100 +1,117 @@
-import React, { useState } from 'react';
-import Helmet from 'react-helmet';
-import { useQuery } from 'react-query';
-import { toast } from 'react-toastify';
-import useUser from '../../../Hooks/useFirebase.js/useUser';
-import Loading from '../../Shared/Loading/Loading';
-import UserDelModal from './UserDelModal';
+import React, { useState } from "react";
+import Helmet from "react-helmet";
+import { useQuery } from "react-query";
+import { toast } from "react-toastify";
+import useUser from "../../../Hooks/useFirebase.js/useUser";
+import Loading from "../../Shared/Loading/Loading";
+import UserDelModal from "./UserDelModal";
 
 const Users = () => {
-    const { handleLogout } = useUser();
-    const [userDel, setUserDel] = useState({});
-    const { data: users, loading, refetch } = useQuery('users', () => fetch('https://young-garden-78103.herokuapp.com/api/users', {
-        method: 'GET',
+  const { handleLogout } = useUser();
+  const [userDel, setUserDel] = useState({});
+  const {
+    data: users,
+    loading,
+    refetch,
+  } = useQuery("users", () =>
+    fetch("http://localhost:5000/api/users/all-users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => {
+      if (res.status === 401 || res.status === 403) {
+        handleLogout();
+        return toast.error("You are not authorized to perform this action");
+      } else {
+        return res.json();
+      }
+    })
+  );
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  const makeAdmin = (email) => {
+    if (email) {
+      fetch(`http://localhost:5000/api/users/make-admin`, {
+        method: "PUT",
         headers: {
-            'Content-Type': 'application/json',
-            "authorization": `Bearer ${localStorage.getItem("accessToken")}`
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
-    }).then(res => {
-        if (res.status === 401 || res.status === 403) {
+        body: JSON.stringify({ email }),
+      })
+        .then((res) => {
+          if (res.status === 401 || res.status === 403) {
             handleLogout();
-            return toast.error('You are not authorized to perform this action');
-        } else {
+            return toast.error("You are not authorized to perform this action");
+          } else {
             return res.json();
-
-        }
-    }));
-
-    if (loading) {
-        return <Loading />
+          }
+        })
+        .then((data) => console.log(data));
+      toast.info("User is now an admin");
+    } else {
+      toast.error("user is not valid");
     }
+    refetch();
+  };
 
-    const makeAdmin = (email) => {
-        if (email) {
-            fetch(`https://young-garden-78103.herokuapp.com/api/admin`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    "authorization": `Bearer ${localStorage.getItem("accessToken")}`
-                },
-                body: JSON.stringify({ email })
-            })
-                .then(res => {
-                    if (res.status === 401 || res.status === 403) {
-                        handleLogout();
-                        return toast.error('You are not authorized to perform this action');
-                    } else {
-                        return res.json();
-
-                    }
-                })
-                .then(data => console.log(data))
-            toast.info('User is now an admin');
-        } else {
-            toast.error('user is not valid');
-        }
-        refetch();
-
-    }
-
-
-    return (
-        <div className="overflow-x-auto">
-            <Helmet>
-                <title>Users</title>
-            </Helmet>
-            {
-                userDel._id && <UserDelModal userDel={userDel} setUserDel={setUserDel} refetch={refetch} />
-            }
-            <table className="table w-full">
-                <thead>
-                    <tr>
-                        <th>user email</th>
-                        <th>role</th>
-                        <th colSpan='2'>action</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {
-                        users?.map(user => {
-                            return (
-                                <tr key={user._id} className="hover">
-                                    <td>{user?.email}</td>
-                                    <td>{user?.isAdmin ? 'admin' : 'client'}</td>
-                                    <td><label htmlFor='user-delete' onClick={() => setUserDel(user)} className='text-red-500 hover:underline hover:cursor-pointer'>Remove user</label></td>
-                                    <td>{
-                                        !user?.isAdmin && <button onClick={() => makeAdmin(user?.email)} className='text-orange-500 hover:underline hover:cursor-pointer'>Make Admin</button>
-                                    }
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="overflow-x-auto">
+      <Helmet>
+        <title>Users</title>
+      </Helmet>
+      {userDel._id && (
+        <UserDelModal
+          userDel={userDel}
+          setUserDel={setUserDel}
+          refetch={refetch}
+        />
+      )}
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>user email</th>
+            <th>role</th>
+            <th colSpan="2">action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user) => {
+            return (
+              <tr key={user._id} className="hover">
+                <td>{user?.email}</td>
+                <td>{user?.is_admin ? "admin" : "client"}</td>
+                <td>
+                  <label
+                    htmlFor="user-delete"
+                    onClick={() => setUserDel(user)}
+                    className="text-red-500 hover:underline hover:cursor-pointer"
+                  >
+                    Remove user
+                  </label>
+                </td>
+                <td>
+                  {!user?.is_admin && (
+                    <button
+                      onClick={() => makeAdmin(user?.email)}
+                      className="text-orange-500 hover:underline hover:cursor-pointer"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Users;
